c2bf6b7b661b1d4660db80dc5db29d15944a1312
gullinhjalti <nils.nahooy@outlook.com>
2018-05-09 15:01:57 +0200

    Updated Venue class to include String mVenueAddress to store the address of the venue in.

--- app/src/main/java/be/runesoft/dev/tourguidelonderzeel/Venue.java
+++ app/src/main/java/be/runesoft/dev/tourguidelonderzeel/Venue.java
@@ -3,17 +3,21 @@ package be.runesoft.dev.tourguidelonderzeel;
 public class Venue {
     private String mVenueName;
     private String mVenueDesc;
+    private String mVenueAddress;
     private static final int NO_IMAGE_PROVIDED = -1;
     private int mVenueImageId;
 
-    Venue(int imageId, String venueName, String venueDesc){
+    Venue(int imageId, String venueName, String venueDesc, String venueAddress) {
         mVenueImageId = imageId;
         mVenueName = venueName;
         mVenueDesc = venueDesc;
+        mVenueAddress = venueAddress;
     }
-    Venue(String venueName, String venueDesc){
+
+    Venue(String venueName, String venueDesc, String venueAddress) {
         mVenueName = venueName;
         mVenueDesc = venueDesc;
+        mVenueAddress = venueAddress;
     }
 
     public boolean hasImage(){
@@ -28,7 +32,11 @@ public class Venue {
         return mVenueName;
     }
 
-    public String getmVenueDesc(){
+    public String getVenueDesc() {
         return mVenueDesc;
+    }
+
+    public String getVenueAddress() {
+        return mVenueAddress;
     }
 }
